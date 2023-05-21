04ad67537abcdfe7fe0b71d4d15ebc42ad069344
Hanna Ponomarenko <hannaponomarenko@ukr.net>
2023-05-22 02:44:29 +0300

    add settings

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { BrowserRouter } from "react-router-dom";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="tech-task">
+    <BrowserRouter basename="test-task-tweet-cards">
       <App />
     </BrowserRouter>
   </React.StrictMode>
